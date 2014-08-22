9bcd253cdd0a87cca2414ecdc92c6458613a0cff
55011212021prince <paphonput@gmail.com>
2014-08-22 16:53:56 +0700

    Prince
    XcodeNew

---
+++ Usernam.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//keep data Array type;
+
+var username = ["prince","arrm"]
+var password = ["1234","4321"]
+
+var usernameDict = ["prince":"1234","arrm":"4321"]
