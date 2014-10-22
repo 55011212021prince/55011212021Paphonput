662bab4f4a31e517b9dec60d3bff647867facb79
55011212021Paphonput <imac@msu>
2014-10-22 16:19:08 +0700

    To be continue
    ColorViewController
    Don’t Finished

---
+++ NavigatorView/NavigatorView/ColorTwoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ColorTwoViewController.swift
+//  NavigatorView
+//
+//  Created by Student on 10/22/14.
+//  Copyright (c) 2014 paphonput. All rights reserved.
+//
+
+import UIKit
+
+class ColorTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NavigatorView/NavigatorView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigatorView
+//
+//  Created by Student on 10/22/14.
+//  Copyright (c) 2014 paphonput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorlabel: UILabel!
+  
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
